cbed05d81b4048b46edf121b601cc223b990d97d
poondog <markj338@gmail.com>
2013-11-23 11:54:38 +1100

    Revert "mm: fix calculation of dirtyable memory"
    This reverts commit d9d85280c0ea5e9a8a5f6de1fa657fc0cec052b7.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -104,26 +104,8 @@ static unsigned long highmem_dirtyable_memory(unsigned long total)
 
 		x += zone_page_state(z, NR_FREE_PAGES) +
 		     zone_reclaimable_pages(z) - z->dirty_balance_reserve;
-        }
-        /*
-         * Unreclaimable memory (kernel memory or anonymous memory
-         * without swap) can bring down the dirtyable pages below
-         * the zone's dirty balance reserve and the above calculation
-         * will underflow.  However we still want to add in nodes
-         * which are below threshold (negative values) to get a more
-         * accurate calculation but make sure that the total never
-         * underflows.
-         */
-        if ((long)x < 0)
-                x = 0;
-
-        /*
-         * Make sure that the number of highmem pages is never larger
-         * than the number of the total dirtyable memory. This can only
-         * occur in very strange VM situations but we want to make sure
-         * that this does not occur.
-         */
-        return min(x, total);
+	}
+	return min(x, total);
 #else
 	return 0;
 #endif
@@ -133,8 +115,8 @@ unsigned long global_dirtyable_memory(void)
 {
 	unsigned long x;
 
-	x = global_page_state(NR_FREE_PAGES) + global_reclaimable_pages();
-	x -= min(x, dirty_balance_reserve);
+	x = global_page_state(NR_FREE_PAGES) + global_reclaimable_pages() -
+	    dirty_balance_reserve;
 
 	if (!vm_highmem_is_dirtyable)
 		x -= highmem_dirtyable_memory(x);
@@ -174,39 +156,13 @@ void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty)
 	trace_global_dirty_state(background, dirty);
 }
 
-/**
- * zone_dirtyable_memory - number of dirtyable pages in a zone
- * @zone: the zone
- *
- * Returns the zone's number of pages potentially available for dirty
- * page cache.  This is the base value for the per-zone dirty limits.
- */
 static unsigned long zone_dirtyable_memory(struct zone *zone)
 {
-        /*
-         * The effective global number of dirtyable pages may exclude
-         * highmem as a big-picture measure to keep the ratio between
-         * dirty memory and lowmem reasonable.
-         *
-         * But this function is purely about the individual zone and a
-         * highmem zone can hold its share of dirty pages, so we don't
-         * care about vm_highmem_is_dirtyable here.
-         */
-        unsigned long nr_pages = zone_page_state(zone, NR_FREE_PAGES) +
-                zone_reclaimable_pages(zone);
-
-        /* don't allow this to underflow */
-        nr_pages -= min(nr_pages, zone->dirty_balance_reserve);
-        return nr_pages;
+	return zone_page_state(zone, NR_FREE_PAGES) +
+	       zone_reclaimable_pages(zone) -
+	       zone->dirty_balance_reserve;
 }
 
-/**
- * zone_dirty_limit - maximum number of dirty pages allowed in a zone
- * @zone: the zone
- *
- * Returns the maximum number of dirty pages allowed in a zone, based
- * on the zone's dirtyable memory.
- */
 static unsigned long zone_dirty_limit(struct zone *zone)
 {
 	unsigned long zone_memory = zone_dirtyable_memory(zone);
