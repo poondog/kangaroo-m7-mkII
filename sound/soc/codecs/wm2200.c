72ef3d2d73e96426e4af1fa287edc92343dd00c5
poondog <markj338@gmail.com>
2013-11-29 00:22:48 +1100

    Revert "Patch to 3.4.29"
    This reverts commit 50a79b8b77229c3fa74a1329ff4dc9f49a17d2e6.

--- sound/soc/codecs/wm2200.c
+++ sound/soc/codecs/wm2200.c
@@ -897,6 +897,8 @@ static const char *wm2200_mixer_texts[] = {
 	"EQR",
 	"LHPF1",
 	"LHPF2",
+	"LHPF3",
+	"LHPF4",
 	"DSP1.1",
 	"DSP1.2",
 	"DSP1.3",
@@ -929,6 +931,7 @@ static int wm2200_mixer_values[] = {
 	0x25,
 	0x50,   /* EQ */
 	0x51,
+	0x52,
 	0x60,   /* LHPF1 */
 	0x61,   /* LHPF2 */
 	0x68,   /* DSP1 */
