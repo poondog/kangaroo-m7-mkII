6eaa97a1473ac50add1f6b5fc09c5c9e0e6ee00d
poondog <markj338@gmail.com>
2013-12-01 21:06:11 +1100

    Revert "Linux 3.4.51"
    This reverts commit 29a52bcacb94a497325bc0cf2e453c12a1193016.

--- fs/notify/inotify/inotify_user.c
+++ fs/notify/inotify/inotify_user.c
@@ -696,10 +696,6 @@ SYSCALL_DEFINE3(inotify_add_watch, int, fd, const char __user *, pathname,
 	int ret, fput_needed;
 	unsigned flags = 0;
 
-	/* don't allow invalid bits: we don't want flags set */
-	if (unlikely(!(mask & ALL_INOTIFY_BITS)))
-		return -EINVAL;
-
 	filp = fget_light(fd, &fput_needed);
 	if (unlikely(!filp))
 		return -EBADF;
